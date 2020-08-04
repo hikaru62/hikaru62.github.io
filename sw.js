45c6745fb2af65a1f89784dcce7f65d135e3a93c
hikaru62 <69145562+hikaru62@users.noreply.github.com>
2020-08-04 09:28:52 +0800

    Add files via upload
    HID Test 1

---
+++ sw.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+toolbox.precache(["index.html","style.css"]);
+
+toolbox.router.get('/images/*', toolbox.cacheFirst);
+
+toolbox.router.get('/*', toolbox.networkFirst, {
+  networkTimeoutSeconds: 5
+});
